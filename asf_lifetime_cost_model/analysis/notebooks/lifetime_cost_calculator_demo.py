feac3d60a5cc5e466f0504b72092d1b5e52432ce
eglucas <elysialucas@gmail.com>
2025-09-15 14:27:08 +0100

    add boiler examples

--- asf_lifetime_cost_model/analysis/notebooks/lifetime_cost_calculator_demo.py
+++ asf_lifetime_cost_model/analysis/notebooks/lifetime_cost_calculator_demo.py
@@ -1,11 +1,16 @@
 # %%
-from asf_lifetime_cost_model.pipeline.lifetime_cost_calculator import LifetimeCostCalculator
+from asf_lifetime_cost_model.pipeline.lifetime_cost_calculator import (
+    LifetimeCostCalculator,
+)
 
 # %%
 calculator = LifetimeCostCalculator()
 
+# %% [markdown]
+# ASHP
+
 # %%
-upfront_costs = calculator.compute_upfront_cost(
+ashp_upfront_costs = calculator.compute_upfront_cost(
     heating_system="ashp",
     annual_cost_reduction=0.05,
     purchase_year=2025,
@@ -15,21 +20,73 @@ upfront_costs = calculator.compute_upfront_cost(
     purchase_with_loan=True,
     loan_interest_rate=0.05,
 )
-print(upfront_costs)
+ashp_upfront_costs
 
 # %%
-maintenance_costs = calculator.compute_total_maintenance_cost(
+ashp_maintenance_costs = calculator.compute_total_maintenance_cost(
     maintenance_frequency_per_year=1, maintenance_cost=100, life_span=10
 )
-print(maintenance_costs)
+ashp_maintenance_costs
 
 
 # %%
-running_costs = calculator.compute_running_cost_time_series(
+ashp_running_costs = calculator.compute_running_cost_time_series(
     purchase_year=2025,
     life_span=10,
     heating_system_efficiency=3,
     fuel_type="electricity",
+    wholesale_price_projection_scenario="reference",
+    include_standing_charge=False,
+    levy_rebalancing=True,
+    levies_to_rebalance=["ro"],
+    levy_rebalancing_weights={
+        "electricity_weight": 0,
+        "gas_weight": 1,
+        "tax_weight": 0,
+        "fixed_electricity_weight": 0,
+        "variable_electricity_weight": 0,
+        "fixed_gas_weight": 0,
+        "variable_gas_weight": 1,
+    },
+    include_vat=True,
+)
+ashp_running_costs
+
+# %%
+ashp_lifetime_costs = calculator.compute_total_lifetime_costs(
+    installation_costs=ashp_upfront_costs,
+    maintenance_costs=ashp_maintenance_costs,
+    running_costs=ashp_running_costs,
+)
+ashp_lifetime_costs
+
+# %% [markdown]
+# Boiler
+
+# %%
+boiler_upfront_costs = calculator.compute_upfront_cost(
+    heating_system="boiler",
+    annual_cost_reduction=0,
+    purchase_year=2025,
+    life_span=10,
+    decile=50,
+    subsidy_model_or_input_values="no subsidy",
+    purchase_with_loan=False,
+)
+boiler_upfront_costs
+
+# %%
+boiler_maintenance_costs = calculator.compute_total_maintenance_cost(
+    maintenance_frequency_per_year=1, maintenance_cost=100, life_span=10
+)
+boiler_maintenance_costs
+
+# %%
+boiler_running_costs = calculator.compute_running_cost_time_series(
+    purchase_year=2025,
+    life_span=10,
+    heating_system_efficiency=0.85,
+    fuel_type="gas",
     wholesale_price_projection_scenario="reference",
     include_standing_charge=True,
     levy_rebalancing=True,
@@ -45,11 +102,14 @@ running_costs = calculator.compute_running_cost_time_series(
     },
     include_vat=True,
 )
-print(running_costs)
+boiler_running_costs
 
 # %%
-calculator.compute_total_lifetime_costs(
-    installation_costs=upfront_costs, maintenance_costs=maintenance_costs, running_costs=running_costs
+boiler_lifetime_costs = calculator.compute_total_lifetime_costs(
+    installation_costs=boiler_upfront_costs,
+    maintenance_costs=boiler_maintenance_costs,
+    running_costs=boiler_running_costs,
 )
+boiler_lifetime_costs
 
 # %%
