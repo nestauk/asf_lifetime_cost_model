665a24b469e6b60f6992b01207be150933fb1923
eglucas <elysialucas@gmail.com>
2026-07-27 15:13:53 +0100

    update demo notebook

--- asf_lifetime_cost_model/analysis/notebooks/model_architecture_demo.py
+++ asf_lifetime_cost_model/analysis/notebooks/model_architecture_demo.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: asf_lifetime_cost_model (3.13.2)
+#     language: python
+#     name: python3
+# ---
+
 # %%
 import asf_lifetime_cost_model.getters.data_getters as data_getters
 from asf_lifetime_cost_model.models.energy_price_trajectory import EnergyPriceTrajectory
@@ -6,37 +23,58 @@ from asf_lifetime_cost_model.models.household import Household
 from asf_lifetime_cost_model.models.installation_cost_trajectory import InstallationCostTrajectory
 from asf_lifetime_cost_model.models.subsidy_trajectory import SubsidyTrajectory
 
+from asf_lifetime_cost_model import config
+
 # %% [markdown]
 # EnergyPriceTrajectory
 
 # %%
-# usage
 electricity_prices = EnergyPriceTrajectory(
-    fuel="electricity", starting_price=data_getters.get_latest_price_cap_rate("electricity")
+    fuel="electricity",
+    starting_price=data_getters.get_latest_price_cap_rate("electricity"),
+    price_basis="real",
+    base_year=2026,
 )
-gas_prices = EnergyPriceTrajectory(fuel="gas", starting_price=data_getters.get_latest_price_cap_rate("gas"))
-
-# electricity_prices.set_trajectory(-0.02, from_year=2027)  # -2% change from 2027
-# electricity_prices.set_trajectory({2030: 10.0})  # explicit override
+electricity_prices.set_trajectory(-0.02, from_year=2027)  # -2%/year, already in real terms
+# electricity_prices.set_trajectory({2030: 10.0})  # explicit override example
 
 electricity_prices
+
+# %%
+gas_prices = EnergyPriceTrajectory(
+    fuel="gas", starting_price=data_getters.get_latest_price_cap_rate("gas"), price_basis="real", base_year=2026
+)
+gas_prices
 
 # %% [markdown]
 # InstallationCostTrajectory
 
 # %%
-gas_boiler_installation_costs = InstallationCostTrajectory(system_type="gas_boiler", starting_cost=3_500)
+boiler_installation_costs = InstallationCostTrajectory(
+    "gas_boiler",
+    starting_cost=3_000,
+    price_basis="real",  # 3,000 is already stated in 2026 real £
+    base_year=2026,
+)
+boiler_installation_costs
+# no set_trajectory call needed
+# the flat starting value across all years already represents "0% real change"
 
 # %%
-ashp_installation_costs = InstallationCostTrajectory(system_type="air_to_water_heat_pump", starting_cost=12_500)
-ashp_installation_costs.set_trajectory(-0.02)
+ashp_installation_costs = InstallationCostTrajectory(
+    system_type="air_to_water_heat_pump", starting_cost=13_100, price_basis="real", base_year=2026
+)
+ashp_installation_costs.set_trajectory(-0.025)
 ashp_installation_costs
+
+# %%
+ashp_installation_costs.cost
 
 # %% [markdown]
 # SubsidyTrajectory
 
 # %%
-ashp_subsidies = SubsidyTrajectory(system_type="air_to_water_heat_pump", starting_subsidy=7_500)
+ashp_subsidies = SubsidyTrajectory(system_type="air_to_water_heat_pump", starting_subsidy=7_500, price_basis="nominal")
 
 # %%
 # example setting trajectory to one of subsidy pathway options
@@ -46,8 +84,11 @@ fast_stepdown_values = {
     int(year): value for year, value in fast_stepdown_row.items() if int(year) in ashp_subsidies.subsidy.index
 }
 ashp_subsidies.set_trajectory(fast_stepdown_values)
-
+ashp_subsidies.to_real(base_year=2026, inflation_rate=config["default_inflation_rate"])
 ashp_subsidies
+
+# %%
+ashp_subsidies.subsidy
 
 # %% [markdown]
 # HeatingSystem
@@ -66,39 +107,70 @@ heat_pump = HeatingSystem(
 heat_pump
 
 # %%
-heat_pump.calculate_annualised_lifetime_cost(heat_demand=13_500, energy_price_trajectory=electricity_prices)
+gas_tdcv = 9_500  # kWh
+heat_demand_uplift_with_heat_pump = 0.08
+heat_demand_with_heat_pump = (9_500 * 0.85) * (1 + heat_demand_uplift_with_heat_pump)
+
 
 # %%
-gas_boiler_subsidies = SubsidyTrajectory(system_type="gas_boiler", starting_subsidy=0.0)
+heat_pump.calculate_annualised_lifetime_cost(
+    heat_demand=heat_demand_with_heat_pump, energy_price_trajectory=electricity_prices
+)
+
+# %%
+heat_pump.calculate_discounted_lifetime_cost(
+    heat_demand=heat_demand_with_heat_pump, energy_price_trajectory=electricity_prices
+)
+
+# %%
+heat_pump.calculate_annualised_discounted_lifetime_cost(
+    heat_demand=heat_demand_with_heat_pump, energy_price_trajectory=electricity_prices
+)
+
+# %%
+gas_boiler_subsidies = SubsidyTrajectory(
+    system_type="gas_boiler", starting_subsidy=0.0, price_basis="real", base_year=2026
+)
 
 gas_boiler = HeatingSystem(
     system_type="gas_boiler",
     installation_year=2026,
     lifespan=15,
     efficiency=0.85,
-    installation_cost_trajectory=gas_boiler_installation_costs,
+    installation_cost_trajectory=boiler_installation_costs,
     subsidy_trajectory=gas_boiler_subsidies,
     maintenance_cost_per_visit=80.0,
     maintenance_annual_frequency=1.0,
 )
 
 # %%
-gas_boiler.calculate_annualised_lifetime_cost(heat_demand=13_500 / 1.08, energy_price_trajectory=gas_prices)
+gas_tdcv = 9_500  # kWh
+heat_demand_with_boiler = 9_500 * 0.85
+
+# %%
+gas_boiler.calculate_annualised_lifetime_cost(heat_demand=heat_demand_with_boiler, energy_price_trajectory=gas_prices)
+
+# %%
+gas_boiler.calculate_discounted_lifetime_cost(heat_demand=heat_demand_with_boiler, energy_price_trajectory=gas_prices)
+
+# %%
+gas_boiler.calculate_annualised_discounted_lifetime_cost(
+    heat_demand=heat_demand_with_boiler, energy_price_trajectory=gas_prices
+)
 
 # %% [markdown]
 # Household
 
 # %%
-# usage
-typical_household_hp = Household(
-    name="Typical household installing an A2W HP", heat_demand=13_500, heating_system=heat_pump
+typical_household_boiler = Household(
+    name="Typical household installing a gas boiler", heat_demand=heat_demand_with_boiler, heating_system=gas_boiler
 )
 
-typical_household_hp.get_annualised_lifetime_cost(energy_price_trajectory=electricity_prices)
+typical_household_boiler.get_annualised_discounted_lifetime_cost(energy_price_trajectory=gas_prices)
 
 # %%
-typical_household_boiler = Household(
-    name="Typical household installing a gas boiler", heat_demand=13_500 / 1.08, heating_system=gas_boiler
+typical_household_heat_pump = Household(
+    name="Typical household installing an A2W HP", heat_demand=heat_demand_with_heat_pump, heating_system=heat_pump
 )
 
-typical_household_boiler.get_annualised_lifetime_cost(energy_price_trajectory=gas_prices)
+typical_household_heat_pump.get_annualised_discounted_lifetime_cost(energy_price_trajectory=electricity_prices)
